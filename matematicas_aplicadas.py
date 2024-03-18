ffa31cfaab8d40335e63e1bac0e322395379c8a3
EduardoRldan <edu.roldan@duocuc.cl>
2024-03-18 17:36:29 -0300

    mensaje

---
+++ matematicas_aplicadas.py
@@ -0,0 +1,68 @@
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append(n-1/n+1)
+    print(f'b{n}={b[i]}')"""
+
+
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append(2/2**2-1)
+    print(f'b{n}={b[i]}')"""
+
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append(n/2)
+    print(f'b{n}={b[i]}')"""
+
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append(n+1/2**n)
+    print(f'b{n}={b[i]}')"""
+
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append(n+2/2*n-1)
+    print(f'b{n}={b[i]}')"""
+
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append(2*n-3)
+    print(f'b{n}={b[i]}')"""
+
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append(1/n)
+    print(f'b{n}={b[i]}')"""
+
+
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append({(-1)**n})
+    print(f'b{n}={b[i]}')"""
+
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append({n+2})
+    print(f'b{n}={b[i]}')"""
+
+
+"""b = []
+for i in range (5):
+    n=i+1
+    b.append({-3*n+11})
+    print(f'b{n}={b[i]}')"""
+
+b = []
+for i in range (5):
+    n=i+1
+    b.append(-5*n+7)
+    print(f'b{n}={b[i]}')
